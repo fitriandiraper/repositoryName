08ee97b59da6ddc773325a18bfd96cca8e67fe98
fitriandiraper <59228133+fitriandiraper@users.noreply.github.com>
2019-12-26 07:16:34 +0700

    Add files via upload

---
+++ 2019_12_25_235532_create_dosens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->char('nid_dosen', 18) ->unique();
+				$table->string('nama', 150);
+			    $table->string('tmpt_lahir', 100);
+				$table->date('tgl_lahir');
+				$table->string('alamat');
+				$table->char('jns_kelamin', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
